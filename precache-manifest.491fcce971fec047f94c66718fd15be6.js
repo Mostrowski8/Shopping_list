867bacf0d135f49fa26f239e4a15dfb8c580d8fa
mostrowski8 <mostrowski8@gmail.com>
2019-04-11 21:44:53 +0200

    Updates

---
+++ precache-manifest.491fcce971fec047f94c66718fd15be6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "371fcda027af4b9065e2",
+    "url": "/Shopping_list/static/js/runtime~main.1e21ae79.js"
+  },
+  {
+    "revision": "4adcf8165d5033bd59e2",
+    "url": "/Shopping_list/static/js/main.b40b5cde.chunk.js"
+  },
+  {
+    "revision": "b337b359586cf0009965",
+    "url": "/Shopping_list/static/js/2.0a19ff11.chunk.js"
+  },
+  {
+    "revision": "4adcf8165d5033bd59e2",
+    "url": "/Shopping_list/static/css/main.f62c8e49.chunk.css"
+  },
+  {
+    "revision": "4af5c925f9ee0032562f00b65cabaa35",
+    "url": "/Shopping_list/index.html"
+  }
+];
